2f3fcc658d8969735c7c3d7fd85f9b0f3be6620d
RV <rvbenu@gmail.com>
2025-02-02 21:14:25 -0500

    Mini-task complete

---
+++ mini-task-testing.py
@@ -0,0 +1,87 @@
+import subprocess
+
+
+"""
+get_pci_ids
+    Return list of PCI IDs in the server. 
+"""
+def get_pci_ids():
+    command = "grep PCI_ID /sys/bus/pci/devices/*/uevent | cut -d'=' -f2"
+    try:
+        process_stdout = subprocess.check_output(
+                command, text=True, shell=True
+            )
+        pci_ids = process_stdout.strip().split("\n")
+        return pci_ids
+    except subprocess.CalledProcessError as e:
+        print(f"Error fetching PCI IDs: {e}")
+        return []
+
+
+"""
+parse_pci_device_name:
+    Returns the name of the device, next to the 'Device' tag in 'stdout,' as a string. 
+    'stdout' is output corresponding to an 'lspci -vmm -d <pci_id>' call. 
+    Returns 'Not found' if nothing was found next to the 'Device' tag.
+"""
+def parse_pci_device_name(stdout):
+    """
+    'lspci -vmm -d <pci_id>' generates machine readable output that is consistent 
+    every time. Thus, it is safe to assume line 4 always contains the device name. 
+    """
+    stdout_lines = stdout.splitlines()
+    return stdout_lines[3]
+   
+
+"""
+get_pci_device_name
+    Returns the device name of a PCI given its vendor and device ID. 
+    Queries locally first. If not found, queries central PCI ID database. 
+     It is recommendable to query the local PCI ID database first 
+    to avoid overloading the central PCI ID database servers. 
+    If PCI not found, returns "Not found"
+"""
+def get_pci_device_name(vendor_id, device_id):
+    pci_id = f"{vendor_id}:{device_id}"
+    
+    command_query_local = f"lspci -vmm -d {pci_id}"
+    command_query_central = f"lspci -q -vmm -d {pci_id}"
+    
+    try: 
+        process_query_local_stdout = subprocess.check_output(
+                command_query_local, text=True, shell=True
+            )
+        if not process_query_local_stdout:
+            print(f"{pci_id} not found in local database. \n")
+        else: 
+            device_name = parse_pci_device_name(process_query_local_stdout)
+            return f"{pci_id}: {device_name}"
+    except subprocess.CalledProcessError as e:
+        print(f"Error querying local database: {e} \n")
+
+    try: 
+        process_query_central_stdout = subprocess.check_output(
+                command_query_central, text=True, shell=True    
+            )
+        if not process_query_central_stdout:
+            print(f"{pci_id} not found in central database. \n")
+        else:
+            device_name = parse_pci_device_name(process_query_central_stdout)
+            return f"{pci_id}: {device_name}"
+    except subprocess.CalledProcessError as e:
+        print(f"Error querying central database: {e} \n")
+   
+    return f"{pci_id} not found. \n"
+
+
+def main():
+    pci_ids = get_pci_ids()
+    for pci_id in pci_ids:
+        vendor_id = pci_id[0:4]
+        device_id = pci_id[5:9]
+        device_name = get_pci_device_name(vendor_id, device_id)
+        print(f"{device_name} \n")
+
+
+if __name__ == '__main__':
+    main()
